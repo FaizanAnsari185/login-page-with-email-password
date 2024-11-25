708429c41714933fdf9a3db91a2a18cb922a7d73
FaizanAnsari185 <superseupermfa@gmail.com>
2024-11-25 19:07:34 +0530

    add password input box eye button

--- Components/Admin.jsx
+++ Components/Admin.jsx
@@ -3,14 +3,29 @@ import React from "react";
 const Admin = () => {
   return (
     <>
-      <div className="h-screen bg-amber-200 flex flex-col gap-10 justify-center items-center">
-        <h1 className="text-3xl text-white bg-purple-800 p-4 text-center rounded-2xl">
-          Hello Faizan🖐️
-        </h1>
-        <h1 className="bg-red-400 text-3xl p-4 rounded-2xl">Welcome Back👍</h1>
-        <h1 className="bg-green-400 text-3xl p-4 rounded-2xl">
-          This Is Admin Page📄
-        </h1>
+      <div className="h-screen w-screen bg-zinc-900 text-white">
+        <nav className="flex justify-between items-center bg-transparent px-20 py-4 border-b-[1px] border-[#4949499a]">
+          <div className="text-2xl font-bold cursor-pointer">
+            Hello Faizan🖐️
+          </div>
+          <div className="capitalize text-xs tracking-widest font-bold flex justify-between items-center gap-16">
+            <h1 className="cursor-pointer">about</h1>
+            <h1 className="cursor-pointer">contact</h1>
+            <h1 className="cursor-pointer">service</h1>
+          </div>
+        </nav>
+        <div className="flex justify-center items-center gap-20 mt-32">
+          <div className="p-24 bg-indigo-950 rounded-xl hover:scale-110 hover:-rotate-3 transition transform ease-in-out duration-500">
+            <h1 className="bg-zinc-900 text-3xl p-8 rounded-2xl hover:rotate-3 hover:scale-110 transition transform ease-in-out duration-500">
+              WelcomeBack👍
+            </h1>
+          </div>
+          <div className="p-24 bg-red-950 rounded-xl hover:scale-110 hover:rotate-3 transition transform ease-in-out duration-500">
+            <h1 className="bg-zinc-900 text-3xl p-8 rounded-2xl hover:-rotate-3 hover:scale-110 transition transform ease-in-out duration-500">
+              This Is Admin Page📄
+            </h1>
+          </div>
+        </div>
       </div>
     </>
   );
--- Components/LoginPage.jsx
+++ Components/LoginPage.jsx
@@ -1,13 +1,20 @@
 "use client";
 import React, { useState } from "react";
+import { TbEyeClosed } from "react-icons/tb";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 const LoginPage = ({ handleLogin }) => {
   const [hint, setHint] = useState(false);
   const [isEmail, setIsEmail] = useState("");
   const [IsPassword, setIsPassword] = useState("");
+  const [eye, setEye] = useState(false);
 
   function handleHint() {
     setHint(!hint);
+  }
+
+  function handleEye() {
+    setEye(!eye);
   }
 
   function handleSubmit(e) {
@@ -15,6 +22,7 @@ const LoginPage = ({ handleLogin }) => {
     handleLogin(isEmail, IsPassword);
     setIsEmail("");
     setIsPassword("");
+    setEye(false)
   }
 
   function handleEmail(e) {
@@ -31,29 +39,34 @@ const LoginPage = ({ handleLogin }) => {
         <form
           onSubmit={handleSubmit}
           className="
-        flex flex-col items-center gap-5 p-10 shadow-2xl rounded-2xl text-white bg-black border-2 border-red-500"
+        flex flex-col items-center gap-5 p-10 shadow-2xl rounded-2xl text-white bg-black border-2 border-blue-500"
         >
-          <h1 className="text-4xl font-bold text-red-500">Login</h1>
+          <h1 className="text-4xl font-bold text-blue-500">Login</h1>
           <p className="text-sm">Enter Your Email or Password</p>
           <input
             onChange={handleEmail}
             value={isEmail}
             type="text"
             placeholder="Email"
-            className="px-3 py-1 text-sm rounded-2xl bg-black focus:outline-none border-2 border-red-500"
+            className="px-5 py-1 text-sm rounded-2xl bg-black focus:outline-none border-2 border-blue-500"
           />
-          <input
-            onChange={handlePassword}
-            value={IsPassword}
-            type="password"
-            placeholder="Password"
-            className="px-3 py-1 text-sm rounded-2xl bg-black focus:outline-none border-2 border-red-500"
-          />
+          <div className="flex items-center px-3 py-1 text-sm rounded-2xl border-2 border-blue-500">
+            <input
+              onChange={handlePassword}
+              value={IsPassword}
+              type={eye ? "text" : "password"}
+              placeholder="Password"
+              className="bg-black focus:outline-none"
+            />
+            <div onClick={handleEye} className="cursor-pointer text-white">
+              {eye ? <TbEyeClosed /> : <MdOutlineRemoveRedEye />}
+            </div>
+          </div>
           <div className="text-center">
             {!hint ? (
               <p
                 onClick={handleHint}
-                className="hover:text-red-500 underline cursor-pointer"
+                className="hover:text-blue-500 underline cursor-pointer"
               >
                 Hint?
               </p>
@@ -63,7 +76,7 @@ const LoginPage = ({ handleLogin }) => {
               </p>
             )}
           </div>
-          <button className="border-[#ff2323] text-white bg-[#ff2323] text-xs py-2 px-8 rounded-2xl">
+          <button className="border-blue-500 text-white bg-blue-500 text-xs py-2 px-8 rounded-2xl">
             Login
           </button>
         </form>
